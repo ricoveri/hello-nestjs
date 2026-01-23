96f7ce1c28a57158e76311cf46c0460a0f7966f9
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2026-01-23 14:06:19 +0100

    feat(database): integrate TypeORM with MySQL for database management
    - Added @nestjs/typeorm package to package.json dependencies.
    - Included mysql2 package for MySQL support.
    - Configured TypeOrmModule in app.module.ts to connect to a MySQL database with auto-loading of entities and synchronization enabled.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 // Main AppModule used by the nest instance created
 // at main.ts
 @Module({
-  imports: [ItemsModule], // Include all resource modules
+  imports: [
+    ItemsModule,
+    // TypeOrm integration goes in here
+    // ------------------------------------
+    TypeOrmModule.forRoot({
+      type: 'mysql', // type of our database
+      host: 'localhost', // database host
+      port: 3306, // database host
+      username: 'todoapp-user', // username
+      password: 'todoapp-passwd-user', // user password (same as the one set in docker-compose.yml)
+      database: 'todoapp', // name of our database,
+      autoLoadEntities: true, // models will be loaded automatically
+      // WARNING: DO NOT USE THIS ONE IN A PRODUCTION ENVIRONMENT
+      synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
+    })], // Include all resource modules
   controllers: [AppController], // FIXME: not sure if we're gonna need this controller
   providers: [AppService], // FIXME: not sure if we're gonna need this service
 })
